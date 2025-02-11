50ef72b3c2092182a2b3fca61f9ad4ed8951a5d7
Julian Lawrance <julian.lawrance@gmail.com>
2025-02-11 13:32:16 +0000

    Improve appearance of score text above the board

--- scripts/score_ui.gd
+++ scripts/score_ui.gd
@@ -15,5 +15,5 @@ func _ready() -> void:
 		board.score_changed.connect(_update_ui)
 
 func _update_ui(black_score: int, white_score: int) -> void:
-	black_score_label.text = "Black: %d" % black_score
-	white_score_label.text = "White: %d" % white_score
+	black_score_label.text = str(black_score)
+	white_score_label.text = str(white_score)
