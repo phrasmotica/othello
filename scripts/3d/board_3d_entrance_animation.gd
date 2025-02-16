68591a18592b6a25d76d0b8e9b79f1c9a1b4d728
Julian L <julian.lawrance@gmail.com>
2025-02-16 21:52:35 +0000

    Create entrance animation for board in 3D game scene

---
+++ scripts/3d/board_3d_entrance_animation.gd
@@ -0,0 +1,45 @@
+class_name Board3DEntranceAnimation extends Node
+
+@export
+var board: Board3D
+
+@export
+var camera_rig: CameraRig
+
+@export
+var game_ui: Control
+
+func run() -> void:
+	if board and camera_rig and game_ui:
+		_prepare_for_entrance()
+
+		var tween := create_tween()
+
+		tween.tween_property(
+			board,
+			"scale",
+			Vector3.ONE,
+			0.5
+		).set_delay(0.5)
+
+		tween.tween_callback(_after_entrance)
+
+		tween.tween_callback(game_ui.show).set_delay(1.0)
+
+func _prepare_for_entrance() -> void:
+	game_ui.hide()
+
+	camera_rig.top_level = true
+	board.scale = 0.01 * Vector3.ONE
+
+	var counters := get_tree().get_nodes_in_group("counters3d")
+	for c in counters:
+		(c as Counter3D).disable_rigid_body()
+
+func _after_entrance() -> void:
+	var counters := get_tree().get_nodes_in_group("counters3d")
+	for c in counters:
+		(c as Counter3D).enable_rigid_body()
+
+	camera_rig.top_level = false
+	board.scale = Vector3.ONE
--- scripts/3d/game_3d.gd
+++ scripts/3d/game_3d.gd
@@ -4,9 +4,18 @@ extends Node3D
 var main_menu_scene: PackedScene = load("res://scenes/3d/main_menu_3d.tscn")
 
 @onready
+var entrance_animation: Board3DEntranceAnimation = %Board3DEntranceAnimation
+
+@onready
 var buttons: GameButtons = %GameButtons
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+
+	if entrance_animation:
+		entrance_animation.run()
+
 	if buttons:
 		buttons.quit_to_main_menu.connect(_handle_quit)
 
