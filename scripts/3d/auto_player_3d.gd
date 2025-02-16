ce767c40ebb59c57b37f30548cbf1ab698bf3492
Julian L <julian.lawrance@gmail.com>
2025-02-16 20:25:06 +0000

    Move main menu's board off-screen before starting the game, and stop the auto-playing also

--- scripts/3d/auto_player_3d.gd
+++ scripts/3d/auto_player_3d.gd
@@ -1,10 +1,12 @@
-extends Node
+class_name AutoPlayer3D extends Node
 
 @export_range(2.0, 5.0)
 var play_interval := 2.0
 
 @export
 var board: Board3D
+
+var _tween: Tween
 
 func _ready() -> void:
 	_start_autoplay()
@@ -13,10 +15,14 @@ func _start_autoplay() -> void:
 	if not board:
 		return
 
-	var tween := create_tween().set_loops()
+	_tween = create_tween().set_loops()
 
-	tween.tween_callback(
+	_tween.tween_callback(
 		func() -> void:
 			if board:
 				board.play_random()
 	).set_delay(play_interval)
+
+func stop_autoplay() -> void:
+	if _tween:
+		_tween.stop()
--- scripts/3d/main_menu_3d.gd
+++ scripts/3d/main_menu_3d.gd
@@ -4,6 +4,12 @@ extends Node3D
 var game_scene: PackedScene = load("res://scenes/3d/game_3d.tscn")
 
 @onready
+var auto_player: AutoPlayer3D = %AutoPlayer3D
+
+@onready
+var camera_rig: CameraRig = %CameraRig
+
+@onready
 var ui: MainMenuUI = %MainMenuUI
 
 func _ready() -> void:
@@ -12,6 +18,21 @@ func _ready() -> void:
 		ui.quit.connect(_handle_quit)
 
 func _handle_started() -> void:
+	auto_player.stop_autoplay()
+	camera_rig.camera_mode = CameraRig.CameraMode.STATIC
+
+	var tween := create_tween()
+
+	tween.tween_property(
+		camera_rig,
+		"position:x",
+		camera_rig.position.x + 30,
+		0.4
+	)
+
+	tween.finished.connect(_load_game)
+
+func _load_game() -> void:
 	if game_scene:
 		Globals.init_finished = false
 		get_tree().change_scene_to_packed(game_scene)
