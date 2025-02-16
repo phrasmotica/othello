9e90775c4914f51cb88285d3dd93d6b497c96663
Julian L <julian.lawrance@gmail.com>
2025-02-16 13:16:09 +0000

    Animate rotation of camera rig in-game

--- scripts/3d/camera_rig.gd
+++ scripts/3d/camera_rig.gd
@@ -15,8 +15,34 @@ var current_position := 0:
 
 		_refresh()
 
+@export
+var use_tweening := false
+
 func _ready() -> void:
 	_refresh()
 
 func _refresh() -> void:
-	rotation_degrees.y = int(current_position * y_interval_degrees) % 360
+	if not Engine.is_editor_hint() and use_tweening:
+		_rotate()
+	else:
+		rotation_degrees.y = int(current_position * y_interval_degrees) % 360
+
+func _rotate() -> void:
+	var current_rotation := rotation_degrees.y
+	var final_rotation := current_position * y_interval_degrees
+
+	print("Rotating camera rig from %.0f to %.0f" % [current_rotation, final_rotation])
+
+	var tween := create_tween()
+
+	tween.tween_property(
+		self,
+		"rotation_degrees:y",
+		final_rotation,
+		0.2
+	)
+
+	tween.tween_callback(
+		func() -> void:
+			print("Finished rotating camera rig from %.0f to %.0f" % [current_rotation, final_rotation])
+	)
