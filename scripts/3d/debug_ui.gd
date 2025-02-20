5d5f393a80903039ca15ecdcb40ba81bb8d4b3de
Julian L <julian.lawrance@gmail.com>
2025-02-20 10:39:19 +0000

    Add coloured background to debug UI. Show it if the board is busy

--- scripts/3d/debug_ui.gd
+++ scripts/3d/debug_ui.gd
@@ -1,15 +1,30 @@
-extends VBoxContainer
+extends MarginContainer
 
 @export
 var board: Board3D
 
 @onready
+var background: Control = %Background
+
+@onready
 var board_busy_label: Label = %BoardBusyLabel
+
+var _board_is_busy := false
 
 func _ready() -> void:
 	if board:
 		SignalHelper.persist(board.busy_changed, _handle_board_busy_changed)
 
+	_refresh()
+
 func _handle_board_busy_changed(is_busy: bool) -> void:
+	_board_is_busy = is_busy
+
+	_refresh()
+
+func _refresh() -> void:
+	if background:
+		background.visible = _board_is_busy
+
 	if board_busy_label:
-		board_busy_label.text = "Board busy: %s" % is_busy
+		board_busy_label.text = "Board busy: %s" % _board_is_busy
