b72c4e7ce25fd9c8deb649b4459a7bcf71f1e1e8
Julian Lawrance <julian.lawrance@gmail.com>
2025-02-20 10:06:09 +0000

    Create basic debug UI scene. It's not connected to any signals yet

--- scripts/3d/debug_handler_3d.gd
+++ scripts/3d/debug_handler_3d.gd
@@ -3,18 +3,31 @@ extends Node
 @export
 var board: Board3D
 
+@export
+var debug_canvas: CanvasLayer
+
 var _is_debug := false
 
 func _ready() -> void:
-	Globals.toggled_debug_mode.connect(_handle_toggled_debug_mode)
-
-func _process(_delta: float) -> void:
-	if not _is_debug:
+	if Engine.is_editor_hint():
 		return
 
+	SignalHelper.persist(Globals.toggled_debug_mode, _handle_toggled_debug_mode)
+
+	_refresh()
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("place_counter_debug"):
 		if board:
 			board.play_random()
 
 func _handle_toggled_debug_mode(is_debug: bool) -> void:
 	_is_debug = is_debug
+
+	_refresh()
+
+func _refresh() -> void:
+	set_process(_is_debug)
+
+	if debug_canvas:
+		debug_canvas.visible = _is_debug
