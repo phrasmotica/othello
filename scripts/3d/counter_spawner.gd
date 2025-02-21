0fde5d12e820ac95ae9196140266f1f26641cf34
Julian L <julian.lawrance@gmail.com>
2025-02-21 23:01:06 +0000

    Apply a downward impulse to each spawned counter

--- scripts/3d/counter_spawner.gd
+++ scripts/3d/counter_spawner.gd
@@ -6,8 +6,11 @@ var counter_scene: PackedScene
 @export
 var spawn_amount := 10
 
-@export_range(0.1, 0.3)
+@export_range(0.05, 0.3)
 var spawn_interval := 0.1
+
+@export_range(10.0, 30.0)
+var spawn_force := 20.0
 
 @export_range(0.01, 0.1)
 var counter_sleep_threshold := 0.05
@@ -24,6 +27,9 @@ func spawn_counters() -> void:
 		var counter: Counter3D = counter_scene.instantiate()
 		counter.position = position + _get_offset()
 		counter.rotation = _get_rotation_offset()
+
+		var rb := counter.get_child(0) as RigidBody3D
+		rb.apply_impulse(spawn_force * Vector3.DOWN)
 
 		SignalHelper.once(
 			counter.landed_on_board,
