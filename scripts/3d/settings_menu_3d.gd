bb7311ac90fca65b64dd2ed63af709b8b85247fb
Julian Lawrance <julian.lawrance@gmail.com>
2025-02-23 21:24:41 +0000

    Add settings menu to 3D game scene. Add game button for showing the settings menu

--- scripts/3d/settings_menu_3d.gd
+++ scripts/3d/settings_menu_3d.gd
@@ -1,1 +1,26 @@
 extends Node3D
+
+@export
+var board: Board3D
+
+@onready
+var settings_menu_ui: SettingsMenuUI = %SettingsMenuUI
+
+func _ready() -> void:
+	if settings_menu_ui:
+		SignalHelper.persist(
+			settings_menu_ui.preview_flips_check_box_toggled,
+			_handle_preview_flips_toggled
+		)
+
+		SignalHelper.persist(
+			settings_menu_ui.close_button_pressed,
+			_handle_close_button_pressed
+		)
+
+func _handle_preview_flips_toggled(toggled_on: bool) -> void:
+	if board:
+		board.show_flip_previews = toggled_on
+
+func _handle_close_button_pressed() -> void:
+	hide()
--- scripts/game_buttons.gd
+++ scripts/game_buttons.gd
@@ -3,24 +3,35 @@ class_name GameButtons extends VBoxContainer
 @export
 var game_logic: OthelloGameLogic
 
+@export
+var settings_menu: Node3D
+
 @onready
 var restart_button: Button = %RestartButton
 
 @onready
+var settings_button: Button = %SettingsButton
+
+@onready
 var main_menu_button: Button = %MainMenuButton
 
-signal restarted
 signal quit_to_main_menu
 
 func _ready() -> void:
+	if settings_menu:
+		settings_menu.hide()
+
 	restart_button.pressed.connect(_handle_restarted)
+	settings_button.pressed.connect(_handle_settings)
 	main_menu_button.pressed.connect(_handle_quit)
 
 func _handle_restarted() -> void:
 	if game_logic:
 		game_logic.restart_game()
 
-	restarted.emit()
+func _handle_settings() -> void:
+	if settings_menu:
+		settings_menu.show()
 
 func _handle_quit() -> void:
 	quit_to_main_menu.emit()
