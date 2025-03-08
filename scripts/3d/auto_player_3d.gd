250b8e52709b3f3fe8d4276cb013cc6985c54aa9
Julian L <julian.lawrance@gmail.com>
2025-03-08 17:35:04 +0000

    Stop auto-player script when the game ends

--- scripts/3d/auto_player_3d.gd
+++ scripts/3d/auto_player_3d.gd
@@ -4,16 +4,24 @@ class_name AutoPlayer3D extends Node
 var play_interval := 2.0
 
 @export
+var game_logic: OthelloGameLogic3D
+
+@export
 var board: Board3D
 
 var _tween: Tween
 
 func _ready() -> void:
+	if game_logic:
+		SignalHelper.persist(game_logic.game_ended, stop_autoplay)
+
 	_start_autoplay()
 
 func _start_autoplay() -> void:
 	if not board:
 		return
+
+	print("Starting autoplay")
 
 	_tween = create_tween().set_loops()
 
@@ -24,5 +32,7 @@ func _start_autoplay() -> void:
 	).set_delay(play_interval)
 
 func stop_autoplay() -> void:
+	print("Stopping autoplay")
+
 	if _tween:
 		_tween.stop()
